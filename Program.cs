5740a0d7f85a4fc0a730071f957323b934919508
Admin <Admin@DESKTOP-VA5AS2M>
2020-09-08 13:40:29 +0700

    Published

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:  ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello : " + name);
+            Console.ReadKey();
+        }
+    }
+}
